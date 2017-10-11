46df2cdc2004f5e2f04c76b7fba0525c584e6fac
Ryan Delaney <vitineth@gmail.com>
2017-10-11 20:56:39 +0100

    Documentation - Tribe.java
    Also added a TODO to implement the cull() method in order to remove dead Krits from the list of Tribe members.

--- src/main/java/com/github/vitineth/game/krit/Tribe.java
+++ src/main/java/com/github/vitineth/game/krit/Tribe.java
@@ -7,11 +7,13 @@ import com.github.vitineth.game.krit.utils.MathUtils;
 
 import java.awt.Color;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.UUID;
 
 /**
- * Class Description
+ * Represents a tribe of Krits. When created, a Krit must be assigned to the tribe as it bases its movement and other
+ * factors around it. Tribes are fairly simple as they just have an id, a set of member krits and a colour.
  * <p/>
  * File created by Ryan (vitineth).<br>
  * Created on 06/10/2017.
@@ -21,33 +23,89 @@ import java.util.UUID;
  */
 public class Tribe {
 
+    /**
+     * The unique identifier for the Tribe. This is designed not to collide with any other Tribe ids but it cannot be
+     * guaranteed.
+     */
     private final UUID id = UUID.randomUUID();
+    /**
+     * The set of members of the Tribe. Each of these refers to a living or dead member of the tribe.
+     * <p/>
+     * TODO:(Ryan Delaney//vitineth@gmail.com): Cull dead members of the tribe from this list.
+     */
     private List<Krit> members = new ArrayList<>();
+    /**
+     * The colour for the tribe. This is also designed to be unique but it does not have to be.
+     */
     private Color color = ColorUtils.newRandomColor();
 
+    /**
+     * Create a new tribe. The ID and colour are randomly generated on instantiation and the members list is created so
+     * no parameters have to be passed in order to create it.
+     */
     public Tribe() {
     }
 
+    /**
+     * Add a member to the tribe
+     *
+     * @param krit {@link Krit} The member of the tribe to add
+     */
     public void add(Krit krit) {
         members.add(krit);
     }
 
-    public boolean containsKrit(Krit krit) {
-        return containsKrit(krit);
+    /**
+     * Culls of dead members of the tribe from the list of members. This is designed to reduce the unnecessary storage
+     * of members of the tribe who no longer should have an effect on the tribe.
+     */
+    public void cull() {
+        Iterator<Krit> k = members.iterator();
+        while (k.hasNext()) {
+            Krit krit = k.next();
+            if (!krit.isAlive() || krit.getHealth() <= 0) k.remove();
+        }
     }
 
+    /**
+     * Tests whether the given krit exists as a member of the tribe
+     *
+     * @param krit {@link Krit} The krit to test
+     * @return true if the given krit exists in the tribe
+     */
+    public boolean containsKrit(Krit krit) {
+        return members.contains(krit);
+    }
+
+    /**
+     * Returns a random member of the tribe. It uses the {@link GlobalStorage#RANDOM} with
+     * {@link java.util.Random#nextInt(int)} to generate using the length of the members list in order to select a
+     * member
+     *
+     * @return {@link Krit} A random member of the tribe
+     */
     public Krit randomSelect() {
         return members.get(GlobalStorage.RANDOM.nextInt(members.size()));
     }
 
+    /**
+     * Removes a given Krit from the list
+     *
+     * @param krit {@link Krit} the krit to remove
+     */
     public void remove(Krit krit) {
-        remove(krit.getId());
+        members.remove(krit);
     }
 
-    public void remove(UUID uuid) {
-        members.remove(uuid);
-    }
-
+    /**
+     * Returns all Krits within the given radius value. This is not the most efficient method of doing it but it will
+     * get all tiles within a 2*radius by 2*radius square and then test each of the points to determine its distance
+     * from the given center point and only return matches that are less than or equal to the radius.
+     *
+     * @param radius int The radius with which the distance from the center to the Krits must be less than or equal to.
+     * @param krit   {@link Krit} The krit to mark as the center
+     * @return {@link Krit}[] All krits within radius of the given krit
+     */
     public Krit[] getWithinRadius(int radius, Krit krit) {
         List<Krit> matches = new ArrayList<>();
         for (int xo = -radius; xo < radius + 1; xo++) {
@@ -65,31 +123,39 @@ public class Tribe {
         return matches.toArray(new Krit[matches.size()]);
     }
 
-    public Krit[] getWithinRadius(int radius, UUID uuid) {
-        Krit krit = KritStorage.resolve(uuid);
-        return getWithinRadius(radius, krit);
-    }
-
+    /**
+     * Return the closest member of the tribe. This will iterate through the entire tribe and therefore when the tribe
+     * size is very large it will take a large amount of time. This will be O(n) for the size of the tribe.
+     * <p/>
+     * This will return null if there are no krits close to it (it is the only member of the tribe)
+     *
+     * @param krit {@link Krit} The krit from which to find the closest other member of the tribe
+     * @return {@link Krit} The closest krit to the given krit or null if none can be found
+     */
     public Krit getClosest(Krit krit) {
         Krit closest = null;
         double closestDistance = -1;
         for (Krit active : members) {
-            int x = krit.getLocation().getX() - active.getLocation().getX();
-            int y = krit.getLocation().getY() - active.getLocation().getY();
-            double distance = MathUtils.pythagoras(x, y);
-            if (distance <= closestDistance || closest == null) {
-                closest = active;
-                closestDistance = distance;
+            if (active != krit) {
+                int x = krit.getLocation().getX() - active.getLocation().getX();
+                int y = krit.getLocation().getY() - active.getLocation().getY();
+                double distance = MathUtils.pythagoras(x, y);
+                if (distance <= closestDistance || closest == null) {
+                    closest = active;
+                    closestDistance = distance;
+                }
             }
         }
 
         return closest;
     }
 
-    public Krit getClosest(UUID uuid) {
-        return getClosest(KritStorage.resolve(uuid));
-    }
-
+    /**
+     * Returns the average location of the tibe. This will sum the x and y of every member of the tribe and then divide
+     * by the number of members. If the tribe is empty it will return an error
+     *
+     * @return {@link} The location of the rounded center of the tribe
+     */
     public Location getAverageLocation() {
         int xTotal = 0;
         int yTotal = 0;
