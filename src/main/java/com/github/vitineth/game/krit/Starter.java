d43afa40eab0d52a62e9e0fea6c91b15e4fe10b9
Ryan Delaney <vitineth@gmail.com>
2017-10-11 17:02:59 +0100

    Wrapping the existing main code in an exception catcher
    This will create a notification message with the stack trace and the exception message so we can do debugging on systems without a command line (such as on the school system)

--- src/main/java/com/github/vitineth/game/krit/Starter.java
+++ src/main/java/com/github/vitineth/game/krit/Starter.java
@@ -10,10 +10,14 @@ import com.github.vitineth.game.krit.utils.Size;
 import com.github.vitineth.game.krit.view.InspectView;
 import com.github.vitineth.game.krit.view.MapView;
 
+import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.plaf.nimbus.NimbusLookAndFeel;
 import java.awt.Color;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,45 +34,65 @@ public class Starter {
 
     public static void main(String[] args) {
         try {
-            UIManager.setLookAndFeel(new DarculaLaf());
+            try {
+                UIManager.setLookAndFeel(new DarculaLaf());
 //            UIManager.setLookAndFeel(new NimbusLookAndFeel());
-        } catch (UnsupportedLookAndFeelException e) {
-            e.printStackTrace();
-        }
-
-        NameStorage.initialise();
-
-        int width = 100;
-        int height = 100;
-
-        KritStorage.initialise(new Size(width, height));
-
-        List<Tribe> tribes = new ArrayList<>();
-        List<Location> tribeLocations = new ArrayList<>();
-        for (int i = 0; i < GlobalStorage.RANDOM.nextInt(5) + 3; i++) {
-            tribes.add(new Tribe());
-            tribeLocations.add(new Location(GlobalStorage.RANDOM.nextInt(width), GlobalStorage.RANDOM.nextInt(height)));
-        }
-
-        for (int i = 0; i < GlobalStorage.RANDOM.nextInt(25) + 25; i++) {
-            int tribeIndex = GlobalStorage.RANDOM.nextInt(tribes.size());
-            Tribe tribe = tribes.get(tribeIndex);
-            Location center = tribeLocations.get(tribeIndex);
-
-            Location newLocation = new Location(center, (int) RandomUtils.random(-20, 20), (int) RandomUtils.random(-20, 20));
-            int count = 0;
-            while(!KritStorage.isFree(newLocation) && count++ <= 20) newLocation = new Location(center, (int) RandomUtils.random(-20, 20), (int) RandomUtils.random(-20, 20));
-
-            if (KritStorage.isFree(newLocation)){
-                Krit child = new Krit(newLocation, 20d, 1.5d,2, Krit.Sex.select(), ColorUtils.newRandomColor(), tribe, null, null, new ArrayList<>());
-                tribe.add(child);
-
+            } catch (UnsupportedLookAndFeelException e) {
+                e.printStackTrace();
             }
-        }
 
-        InspectView view = new InspectView();
-        view.update(KritStorage.getCreatures().get(0));
-        new Thread(new MapView(view), "Map Viewer").start();
+            NameStorage.initialise();
+
+            int width = 100;
+            int height = 100;
+
+            KritStorage.initialise(new Size(width, height));
+
+            List<Tribe> tribes = new ArrayList<>();
+            List<Location> tribeLocations = new ArrayList<>();
+            for (int i = 0; i < GlobalStorage.RANDOM.nextInt(5) + 3; i++) {
+                tribes.add(new Tribe());
+                tribeLocations.add(new Location(GlobalStorage.RANDOM.nextInt(width), GlobalStorage.RANDOM.nextInt(height)));
+            }
+
+            for (int i = 0; i < GlobalStorage.RANDOM.nextInt(25) + 25; i++) {
+                int tribeIndex = GlobalStorage.RANDOM.nextInt(tribes.size());
+                Tribe tribe = tribes.get(tribeIndex);
+                Location center = tribeLocations.get(tribeIndex);
+
+                Location newLocation = new Location(center, (int) RandomUtils.random(-20, 20), (int) RandomUtils.random(-20, 20));
+                int count = 0;
+                while (!KritStorage.isFree(newLocation) && count++ <= 20)
+                    newLocation = new Location(center, (int) RandomUtils.random(-20, 20), (int) RandomUtils.random(-20, 20));
+
+                if (KritStorage.isFree(newLocation)) {
+                    Krit child = new Krit(newLocation, 20d, 1.5d, 2, Krit.Sex.select(), ColorUtils.newRandomColor(), tribe, null, null, new ArrayList<>());
+                    tribe.add(child);
+
+                }
+            }
+
+            InspectView view = new InspectView();
+            view.update(KritStorage.getCreatures().get(0));
+            new Thread(new MapView(view), "Map Viewer").start();
+        } catch (Exception e) {
+            StringBuilder sb = new StringBuilder();
+            e.printStackTrace(new PrintWriter(new Writer() {
+                @Override
+                public void write(char[] cbuf, int off, int len) throws IOException {
+                    sb.append(cbuf, off, len);
+                }
+
+                @Override
+                public void flush() throws IOException {
+                }
+
+                @Override
+                public void close() throws IOException {
+                }
+            }));
+            JOptionPane.showMessageDialog(null, sb.toString(), "Exception: " + e.getMessage(), JOptionPane.ERROR_MESSAGE);
+        }
     }
 
 }
